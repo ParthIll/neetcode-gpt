cd5f90efa52ddc4d8a83c5782038b686ee5e3760
ParthIll <146685408+ParthIll@users.noreply.github.com>
2026-06-18 09:42:50 -0500

    Implement Layer normalization (PyTorch 2/4)

---
+++ model/normalization.py
@@ -0,0 +1,16 @@
+import numpy as np
+from numpy.typing import NDArray
+
+
+class Solution:
+    def forward(self, x: NDArray[np.float64], gamma: NDArray[np.float64], beta: NDArray[np.float64]) -> NDArray[np.float64]:
+        # x: 1D feature vector
+        # gamma: 1D scale parameter (same length as x)
+        # beta: 1D shift parameter (same length as x)
+        eps = 1e-5
+        mean = 1/len(x)*np.sum(x)
+        var = 1/len(x)*np.sum((x-mean)**2)
+        x_hat = (x - mean) / np.sqrt(var + eps)
+        out = gamma * x_hat + beta
+        return np.round(out, 5)
+        
